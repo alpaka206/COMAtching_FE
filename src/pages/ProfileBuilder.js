f7cf96721776dc8e30c5f00dc70c3e8310e8a37c
alpaka206 <gyuwon05@gmail.com>
2024-04-30 01:54:19 +0900

    feat. add Answer logic at ProfileBuilder

--- src/pages/ProfileBuilder.js
+++ src/pages/ProfileBuilder.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import "../css/pages/ProfileBuilder.css";
 import HeaderNav from "../components/HeaderNav";
 import Footer from "../components/Footer";
@@ -13,13 +13,22 @@ function ProfileBuilder() {
     false,
     false,
   ]);
-
+  const [showAnswers] = useState([
+    ["1사교적이고 활발한 편", "1내성적이고 조용한 편"],
+    ["2사교적이고 활발한 편", "2내성적이고 조용한 편"],
+    ["3사교적이고 활발한 편", "3내성적이고 조용한 편"],
+    ["4사교적이고 활발한 편", "4내성적이고 조용한 편"],
+    ["5사교적이고 활발한 편", "5내성적이고 조용한 편"],
+    ["6사교적이고 활발한 편", "6내성적이고 조용한 편"],
+  ]);
+  const [questionNum, setQuestionNum] = useState(0);
   const handleQuestionComplete = (index) => {
     setShowQuestions((prevShowQuestions) => {
       const updatedQuestions = [...prevShowQuestions];
       updatedQuestions[index] = true;
       return updatedQuestions;
     });
+    setQuestionNum((prevQuestionNum) => index);
   };
   const chatMessageRef = useRef(null);
   useEffect(() => {
@@ -44,85 +53,152 @@ function ProfileBuilder() {
           />
         </div>
         {showQuestions[0] && (
-          <div className="ProfileBuilder">
-            <TypeAnimation
-              sequence={[
-                "Q1. 당신은 사용자와 어떻게 상호작용 하시나요?",
-                1000,
-                () => handleQuestionComplete(1),
-              ]}
-              speed={65}
-              className="typing-animation"
-              cursor={false}
-            />
-          </div>
+          <Fragment>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[
+                  "Q1. 당신은 사용자와 어떻게 상호작용 하시나요?",
+                  1000,
+                ]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+          </Fragment>
         )}
         {showQuestions[1] && (
-          <div className="ProfileBuilder">
-            <TypeAnimation
-              sequence={[
-                "Q2. 당신은 사용자와 어떻게 상호작용 하시나요?",
-                1000,
-                () => handleQuestionComplete(2),
-              ]}
-              speed={65}
-              className="typing-animation"
-              cursor={false}
-            />
-          </div>
+          <Fragment>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[showAnswers[questionNum - 1][0], 1000]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[
+                  "Q2. 당신은 사용자와 어떻게 상호작용 하시나요?",
+                  1000,
+                ]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+          </Fragment>
         )}
         {showQuestions[2] && (
-          <div className="ProfileBuilder">
-            <TypeAnimation
-              sequence={[
-                "Q3. 당신은 사용자와 어떻게 상호작용 하시나요?",
-                1000,
-                () => handleQuestionComplete(3),
-              ]}
-              speed={65}
-              className="typing-animation"
-              cursor={false}
-            />
-          </div>
+          <Fragment>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[showAnswers[questionNum - 1][0], 1000]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[
+                  "Q3. 당신은 사용자와 어떻게 상호작용 하시나요?",
+                  1000,
+                ]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+          </Fragment>
         )}
         {showQuestions[3] && (
-          <div className="ProfileBuilder">
-            <TypeAnimation
-              sequence={[
-                "Q4. 당신은 사용자와 어떻게 상호작용 하시나요?",
-                1000,
-                () => handleQuestionComplete(4),
-              ]}
-              speed={65}
-              className="typing-animation"
-              cursor={false}
-            />
-          </div>
+          <Fragment>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[showAnswers[questionNum - 1][0], 1000]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[
+                  "Q4. 당신은 사용자와 어떻게 상호작용 하시나요?",
+                  1000,
+                ]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+          </Fragment>
         )}
         {showQuestions[4] && (
-          <div className="ProfileBuilder">
-            <TypeAnimation
-              sequence={[
-                "Q5. 당신은 사용자와 어떻게 상호작용 하시나요?",
-                1000,
-                () => handleQuestionComplete(5),
-              ]}
-              speed={65}
-              className="typing-animation"
-              cursor={false}
-            />
-          </div>
+          <Fragment>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[showAnswers[questionNum - 1][0], 1000]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[
+                  "Q5. 당신은 사용자와 어떻게 상호작용 하시나요?",
+                  1000,
+                ]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+          </Fragment>
         )}
         {showQuestions[5] && (
-          <div className="ProfileBuilder">
-            <TypeAnimation
-              sequence={["Q6. 당신은 사용자와 어떻게 상호작용 하시나요?", 1000]}
-              speed={65}
-              className="typing-animation"
-              cursor={false}
-            />
-          </div>
+          <Fragment>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[showAnswers[questionNum - 1][0], 1000]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+            <div className="ProfileBuilder">
+              <TypeAnimation
+                sequence={[
+                  "Q6. 당신은 사용자와 어떻게 상호작용 하시나요?",
+                  1000,
+                ]}
+                speed={65}
+                className="typing-animation"
+                cursor={false}
+              />
+            </div>
+          </Fragment>
         )}
+      </div>
+      <div className="ProfileMaker">
+        <div className="ProfileMaker-text">내 답변</div>
+        <button className="profilemaker-choose-button">
+          <div className="profilemaker-button-title">E</div>
+          <div>{showAnswers[questionNum][0]}</div>
+        </button>
+        <button className="profilemaker-choose-button">
+          <div className="profilemaker-button-title">I</div>
+          <div>{showAnswers[questionNum][1]}</div>
+        </button>
+        <button
+          className="profilemaker-submit-button"
+          onClick={() => handleQuestionComplete(questionNum + 1)}
+        >
+          전송
+        </button>
       </div>
       <Footer />
     </div>
