13c24d01a9fd8b2ab52f8bfd267ce12b87342863
alpaka206 <gyuwon05@gmail.com>
2024-05-30 00:13:05 +0900

    feat. fix url

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -8,10 +8,10 @@ function AdminLogin() {
   return (
     <div className="container">
       <div className="content">
-        <div className="AdminLogin" onClick={() => navigate("/Admin")}>
+        <div className="AdminLogin" onClick={() => navigate("/admin")}>
           관리자 페이지
         </div>
-        <div className="AdminLogin" onClick={() => navigate("/CodeReader")}>
+        <div className="AdminLogin" onClick={() => navigate("/code-reader")}>
           뽑기페이지
         </div>
       </div>
